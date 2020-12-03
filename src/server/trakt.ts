cbc8fd27ff82066fc196de29829b8220c008f066
viusalglitch91 <visualglitch91@protonmail.com>
2020-12-02 22:04:01 -0300

    Cache trakt result and filter out watched episodes

--- src/server/trakt.ts
+++ src/server/trakt.ts
@@ -7,6 +7,8 @@ import rootdir from "./rootdir";
 const traktClient: any = new Trakt(require("./secrets.json").TRAKT);
 const tokenFile = path.join(rootdir, "trakt_token.json");
 
+let upcomingPromise: Promise<any>;
+
 function refreshTokens() {
   const storedTokens = JSON.parse(fs.readFileSync(tokenFile, "utf-8"));
 
@@ -15,33 +17,53 @@ function refreshTokens() {
   });
 }
 
-function getUpcomingShows(): Promise<Episode[]> {
+function refreshUpcoming() {
   const startDate = new Date();
   startDate.setDate(startDate.getDate() - 7);
 
-  return traktClient.calendars.my
-    .shows({
+  upcomingPromise = Promise.all([
+    traktClient.sync.watched({ type: "shows" }),
+    traktClient.calendars.my.shows({
       start_date: startDate.toISOString().slice(0, 10),
       days: "14",
       extended: "full",
-    })
-    .then((episodes: any) =>
-      episodes.map(({ first_aired, episode, show }: any) => ({
+    }),
+  ]).then(([watched, episodes]: any) => {
+    const watchedMap: { [key: string]: boolean } = {};
+
+    watched.forEach((item: any) => {
+      item.seasons.forEach((season: any) => {
+        season.episodes.forEach((episode: any) => {
+          const key = `${item.show.ids.trakt}_${season.number}_${episode.number}`;
+          watchedMap[key] = true;
+        });
+      });
+    });
+
+    return episodes
+      .filter((item: any) => {
+        const key = `${item.show.ids.trakt}_${item.episode.season}_${item.episode.number}`;
+        return !watchedMap[key];
+      })
+      .map(({ first_aired, episode, show }: any) => ({
         date: first_aired,
         show: show.title,
         season: episode.season,
         number: episode.number,
         title: episode.title,
-      }))
-    );
+      }));
+  });
 }
 
 function trakt(app: Express) {
   refreshTokens();
   setInterval(refreshTokens, 24 * 60 * 60 * 1000);
 
+  refreshUpcoming();
+  setInterval(refreshUpcoming, 12 * 60 * 60 * 1000);
+
   app.get("/trakt/upcoming-shows", (_, res) => {
-    getUpcomingShows().then((episodes) => res.send(episodes));
+    upcomingPromise.then((upcoming) => res.send(upcoming));
   });
 }
 
